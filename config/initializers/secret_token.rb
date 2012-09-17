3f0d17e96ba73762135467abb493317112bfbe94
1334 <1334@shadowproject.net>
2012-09-17 13:11:38 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GlobalAib::Application.config.secret_token = '96b5adbeca89291252159b17fb091a1034471b18eeff9fd26ea23ea5b019d2c75eba110549f863a344e7a7ab7cc37fc53721357b220187454897c6edfb1331c1'
